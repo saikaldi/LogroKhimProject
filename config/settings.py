636c337f4e50bd8c547e5b5d14197193015ad63e
Saikal Ulukbekova <ukyzsaikal@gmail.com>
2024-12-03 08:00:20 +0100

    Secret key

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = "4j&=k#om-m#k(r7@03(783p9_o%bvkc)ws398axj#fh8qqk_s="
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 

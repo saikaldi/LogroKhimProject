a4c0c5e9d70611ba7270cb5affc89f1127fd9fc6
islamnurdinov <nurdinovislam4@gmail.com>
2024-12-04 14:47:24 +0600

    Перенастроил настройки

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,6 @@ from rest_framework.authtoken import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('api-token-auth/', auth_views.obtain_auth_token),
     path("admin/", admin.site.urls),
@@ -33,3 +32,6 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

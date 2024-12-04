7d0e7c962df1522f4ead0d34163da9254219076a
islamnurdinov <nurdinovislam4@gmail.com>
2024-12-04 17:28:58 +0600

    Перенастроил настройки

--- users/models.py
+++ users/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
 from django.utils.translation import gettext as _

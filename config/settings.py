ff4d842a67b173215c2d7b0a557ef5c86062c083
islamnurdinov <nurdinovislam4@gmail.com>
2024-12-05 15:10:32 +0600

    Перенастроил настройки

--- config/settings.py
+++ config/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'rest_framework',
     'rest_framework.authtoken',
+    "drf_yasg",
     'users',
     'catalog',
     'carts',
--- config/urls.py
+++ config/urls.py
@@ -17,9 +17,26 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework.authtoken import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documentation",
+        default_version='v1',
+        description="API documentation for my project",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@myproject.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),  # Установите разрешение на AllowAny
+
+)
 
 urlpatterns = [
     path('api-token-auth/', auth_views.obtain_auth_token),
@@ -28,6 +45,8 @@ urlpatterns = [
     path('api/v1/catalog/', include('catalog.urls')),
     path('api/v1/', include('carts.urls')),
     path('api/v1/', include('favorites.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:
